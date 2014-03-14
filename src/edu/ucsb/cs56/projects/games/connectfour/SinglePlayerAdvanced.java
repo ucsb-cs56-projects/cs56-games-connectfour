0ef30031f895b79dcf77582ad867ce06c983933a
Girish Kowligi <gkowligi@umail.ucsb.edu>
2014-03-14 16:13:08 -0700

    Split code. Also, added Advanced computer player

---
+++ src/edu/ucsb/cs56/projects/games/connectfour/SinglePlayerAdvanced.java
@@ -0,0 +1,243 @@
+package edu.ucsb.cs56.projects.games.connectfour;
+
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+class SinglePlayerAdvanced {
+
+    
+    //Check if possibility for player to win
+    //If so, block spot. If not, random move
+    // For Advanced Mode
+    
+    public static void AdvancedComputerMove(Board b){
+        if (!b.getGameOver()){    //make sure game is not already over
+
+            Random rand = new Random();
+            int xIndex = 0;
+            int yIndex = 0;
+	    int weight = 0;
+
+
+
+
+//Horizontal left to right
+        for (int row = 0; row < 6; row++) {
+            for (int col = 0; col < 4; col++) {
+                
+                int curr = b.getGameGridCircle(col, row).getState();
+
+                if (curr > 0
+                    && curr == b.getGameGridCircle(col+1, row).getState() 
+                    && curr == b.getGameGridCircle(col+2, row).getState() 
+		    && (b.getGameGridCircle(col+3, row).getState() == 0) ) 
+                {
+ 	            xIndex = col + 3;
+	            yIndex = row;
+
+            while((yIndex != b.numRows - 1) && (b.getGameGridCircle(xIndex, yIndex+1).getState() == 0)){
+                yIndex++;
+                if (yIndex == b.numRows - 1) {
+                    break;
+                }
+            }
+
+		    weight = 1;
+                    break;
+                }
+            }
+        }
+
+//Horizontal right to left
+        for (int row = 0; row < 6; row++) {
+            for (int col = 6; col > 2; col--) {
+                
+                int curr = b.getGameGridCircle(col, row).getState();
+
+                if (curr > 0
+                    && curr == b.getGameGridCircle(col-1, row).getState() 
+                    && curr == b.getGameGridCircle(col-2, row).getState() 
+		    && (b.getGameGridCircle(col-3, row).getState() == 0) ) 
+                {
+	            xIndex = col - 3;
+	            yIndex = row;
+
+            while((yIndex != b.numRows - 1) && (b.getGameGridCircle(xIndex, yIndex+1).getState() == 0)){
+                yIndex++;
+                if (yIndex == b.numRows - 1) {
+                    break;
+                }
+            }
+
+		    weight = 1;
+                    break;
+                }
+            }
+        }
+        
+//vertical
+        for (int col = 0; col < 7; col++) {
+            for (int row = 5; row > 2; row--) {
+                int curr = b.getGameGridCircle(col, row).getState();
+                if (curr > 0 
+                    && curr == b.getGameGridCircle(col, row-1).getState() 
+                    && curr == b.getGameGridCircle(col, row-2).getState()
+		    && (b.getGameGridCircle(col, row-3).getState() == 0) )
+                {
+	            xIndex = col;
+	            yIndex = row - 3;
+
+            while((yIndex != b.numRows - 1) && (b.getGameGridCircle(xIndex, yIndex+1).getState() == 0)){
+                yIndex++;
+                if (yIndex == b.numRows - 1) {
+                    break;
+                }
+            }
+
+		    weight = 1;
+                    break;
+                }
+            }
+        }
+        
+        
+// diagonal check upper left to lower right, left to right
+        for (int col = 0; col < 4; col++) {
+            for (int row = 0; row < 3; row++) {
+                int curr = b.getGameGridCircle(col, row).getState();
+                if (curr > 0
+                    && curr == b.getGameGridCircle(col+1, row+1).getState() 
+                    && curr == b.getGameGridCircle(col+2, row+2).getState()
+		    && (b.getGameGridCircle(col+3, row+3).getState() == 0) )
+                {
+                    xIndex = col + 3;
+                    yIndex = row + 3;
+
+            while((yIndex != b.numRows - 1) && (b.getGameGridCircle(xIndex, yIndex+1).getState() == 0)){
+                yIndex++;
+                if (yIndex == b.numRows - 1) {
+                    break;
+                }
+            }
+
+		    weight = 1;
+                    break;
+                }
+            }
+        }
+
+// diagonal check upper left to lower right, right to left
+        for (int col = 6; col > 2; col--) {
+            for (int row = 5; row > 2; row--) {
+                int curr = b.getGameGridCircle(col, row).getState();
+                if (curr > 0
+                    && curr == b.getGameGridCircle(col-1, row-1).getState()
+                    && curr == b.getGameGridCircle(col-2, row-2).getState()
+		    && (b.getGameGridCircle(col-3, row-3).getState() == 0) )
+                {
+                    xIndex = col - 3;
+                    yIndex = row - 3;
+
+            while((yIndex != b.numRows - 1) && (b.getGameGridCircle(xIndex, yIndex+1).getState() == 0)){
+                yIndex++;
+                if (yIndex == b.numRows - 1) {
+                    break;
+                }
+            }
+
+		    weight = 1;
+                    break;
+                }
+            }
+        }
+        
+// diagonal upper right to lower left, left to right
+        for (int col = 6; col >= 3; col--) {
+            for (int row = 0; row < 3; row++) {
+                int curr = b.getGameGridCircle(col, row).getState();
+                if (curr > 0
+                    && curr == b.getGameGridCircle(col-1, row+1).getState() 
+                    && curr == b.getGameGridCircle(col-2, row+2).getState()
+		    && (b.getGameGridCircle(col-3, row+3).getState() == 0) ) 
+                {
+                    xIndex = col - 3;
+                    yIndex = row + 3;
+
+            while((yIndex != b.numRows - 1) && (b.getGameGridCircle(xIndex, yIndex+1).getState() == 0)){
+                yIndex++;
+                if (yIndex == b.numRows - 1) {
+                    break;
+                }
+            }
+
+		    weight = 1;
+                    break;
+                }
+            }
+        }
+
+
+// diagonal upper right to lower left, right to left
+        for (int col = 0; col < 4; col++) {
+            for (int row = 5; row > 2; row--) {
+                int curr = b.getGameGridCircle(col, row).getState();
+                if (curr > 0
+                    && curr == b.getGameGridCircle(col+1, row-1).getState() 
+                    && curr == b.getGameGridCircle(col+2, row-2).getState()
+		    && (b.getGameGridCircle(col+3, row-3).getState() == 0) ) 
+                {
+                    xIndex = col + 3;
+                    yIndex = row - 3;
+
+            while((yIndex != b.numRows - 1) && (b.getGameGridCircle(xIndex, yIndex+1).getState() == 0)){
+                yIndex++;
+                if (yIndex == b.numRows - 1) {
+                    break;
+                }
+            }
+
+		    weight = 1;
+                    break;
+                }
+            }
+        }
+
+
+
+	if(weight == 0)
+	{
+		xIndex = rand.nextInt(7);
+
+            // make sure random column is not already full
+            while (b.getGameGridCircle(xIndex,yIndex).getState() != 0) {
+                xIndex = (xIndex + 1)%7;
+            }
+            //find the lowest empty slot in the chosen column
+            //Place a circle there
+            while((yIndex != b.numRows - 1) && (b.getGameGridCircle(xIndex, yIndex+1).getState() == 0)){
+                yIndex++;
+                if (yIndex == b.numRows - 1) {
+                    break;
+                }
+            }
+    
+	}
+
+
+            b.getGameGridCircle(xIndex,yIndex).setState(b.getTurn());
+            b.setTurn(1);
+            b.repaint();
+            b.setDrawCounter(b.getDrawCounter() + 1);
+        }
+    }
+
+}
+
+
+
+
+
--- src/edu/ucsb/cs56/projects/games/connectfour/startScreen2.java
+++ src/edu/ucsb/cs56/projects/games/connectfour/startScreen2.java
@@ -1,5 +1,6 @@
 package edu.ucsb.cs56.projects.games.connectfour;
 
+
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
@@ -8,17 +9,15 @@ import java.util.Random;
 
 /**
  startScreen2 class uses swing gui to represent the Connect 4 start Menu
- 
  @author Vincent Tan
  @author Girish Kowligi
  @version Project1, CS56, W14
- 
-*/
+ */
 
 
 public class startScreen2 extends JFrame {
     
-  
+    
     
     public static int frame_width = 880;
     public static int frame_height = 650;
@@ -29,29 +28,29 @@ public class startScreen2 extends JFrame {
     private int gameMode = 1;
     private static singlePlayerMenuPanel SPMenu;
     private static inGameMenuPanel inGameMenuP;
-    
 
+    
+    
     // Launch game
     public static void main (String [] args){
         JFrame frame = new startScreen2();
-     
+        
     }
-     
+    
     /**
      Constructor intitializes JFrame For all panel
+     */
     
-    */
-
     // initial screen when program is executed
     public startScreen2(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(menu_width,menu_height);
         this.setResizable(false);
-        ss = new StartScreenButtonsPanel();
+        ss = new StartScreenButtonsPanel(this);
         this.add(ss);
         this.addMouseListener(new MouseClass());
         this.setVisible(true);
-
+        
         
     }
     
@@ -61,14 +60,14 @@ public class startScreen2 extends JFrame {
         this.setSize(menu_width,menu_height);
         this.remove(ss);
         this.repaint();
-        SPMenu = new singlePlayerMenuPanel();
+        SPMenu = new singlePlayerMenuPanel(this);
         this.add(SPMenu);
         this.revalidate();
         
         
     }
     /**
-       Navigate Back to the main Menu
+     Navigate Back to the main Menu
      */
     public void BackToStartScreen(){
         if (b != null)
@@ -90,27 +89,27 @@ public class startScreen2 extends JFrame {
     // and make a new one
     public void launchGame(){
         // Make sure Panel already Exist.
-	// remove if it does.
-	if (b != null)
-	    this.remove(b);
+        // remove if it does.
+        if (b != null)
+            this.remove(b);
         if (inGameMenuP != null)
-	    this.remove(inGameMenuP);
+            this.remove(inGameMenuP);
         this.repaint();
-
-	// set the Game size ready for The board
+        
+        // set the Game size ready for The board
         this.setSize(frame_width,frame_height);
         // Remove SinglePlayer Menu if it exist
-	if (SPMenu != null)
+        if (SPMenu != null)
             this.remove(SPMenu);
         // remove startScreen if it exist
-	if (ss!= null)
-	    this.remove(ss);
+        if (ss!= null)
+            this.remove(ss);
         this.setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
-	// initiate a new board and a in-Game Menu
-        inGameMenuP = new inGameMenuPanel();
+        // initiate a new board and a in-Game Menu
+        inGameMenuP = new inGameMenuPanel(this);
         b = new Board();
         
-	// add it to frame and refresh
+        // add it to frame and refresh
         this.add(b);
         this.add(inGameMenuP);
         this.revalidate();
@@ -119,7 +118,7 @@ public class startScreen2 extends JFrame {
         
     }
     
- 
+    
     class MouseClass implements MouseListener{
         private int xIndex;
         private int yIndex;
@@ -131,117 +130,58 @@ public class startScreen2 extends JFrame {
             System.out.println("GameMode is " + gameMode);
             if (gameMode == 1){// multiplayer
                 b.setSinglePlayer(false);
-		xIndex = e.getX()/100;
-		yIndex = 0;
-            
-            
-		while(b.getGameGridCircle(xIndex, yIndex+1).getState() == 0)
-                    {
-                        yIndex++;
-                        if (yIndex == b.numRows - 1) {
-                            break;
-                        }
+                xIndex = e.getX()/100;
+                yIndex = 0;
+                
+                
+                while(b.getGameGridCircle(xIndex, yIndex+1).getState() == 0)
+                {
+                    yIndex++;
+                    if (yIndex == b.numRows - 1) {
+                        break;
                     }
-            
-		//if the top circle is already filled, do nothing and return
-		if (yIndex == 0 && b.getGameGridCircle(xIndex, yIndex).getState() != 0) {
-		    return;
-		}
-            
-		//set the selected circle's state to current turn value (1 or 2)
-                    
-		b.getGameGridCircle(xIndex, yIndex).setState(b.getTurn());
-            
-		//change turns
-		if (b.getTurn() == 1) {
-		    b.setTurn(2);
-		}
-		else {
-		    b.setTurn(1);
-		}
-            
-		//repaint after every mouseClick
-		b.repaint();
-            
-		b.setDrawCounter(b.getDrawCounter()+1);
-            
+                }
+                
+                //if the top circle is already filled, do nothing and return
+                if (yIndex == 0 && b.getGameGridCircle(xIndex, yIndex).getState() != 0) {
+                    return;
+                }
+                
+                //set the selected circle's state to current turn value (1 or 2)
+                
+                b.getGameGridCircle(xIndex, yIndex).setState(b.getTurn());
+                
+                //change turns
+                if (b.getTurn() == 1) {
+                    b.setTurn(2);
+                }
+                else {
+                    b.setTurn(1);
+                }
+                
+                //repaint after every mouseClick
+                b.repaint();
+                
+                b.setDrawCounter(b.getDrawCounter()+1);
+                
             }
             
             // single player easy
-            else if (gameMode == 2){
+            else{
+                
                 b.setSinglePlayer(true);
-		if (b.getTurn() == 1){
-		    xIndex = e.getX()/100;
-		    yIndex = 0;
-                        
-                        
-		    while(b.getGameGridCircle(xIndex, yIndex+1).getState() == 0)
-                        {
-                            yIndex++;
-                            if (yIndex == b.numRows - 1) {
-                                break;
-                            }
-                        }
-                        
-		    //if the top circle is already filled, do nothing and return
-		    if (yIndex == 0 && b.getGameGridCircle(xIndex, yIndex).getState() != 0) {
-			return;
-		    }
-                        
-		    //set the selected circle's state to current turn value (1 or 2)
-                        
-		    b.getGameGridCircle(xIndex, yIndex).setState(b.getTurn());
-                        
-		    //change turns
-		    b.setTurn(2);
-                        
-		    //repaint after every mouseClick
-		    b.repaint();
-                        
-		    b.setDrawCounter(b.getDrawCounter()+1);
-                    
-                                        
-		    //Make a automatic mouse click to start the Computer Move
-		    try {
-			//	Thread.sleep(1000);
-			Robot r = new Robot();
-			r.mousePress(InputEvent.BUTTON1_MASK);
-			r.mouseRelease(InputEvent.BUTTON1_MASK);
-                        
-		    }catch (AWTException ex){
-			System.out.println("didn't work");
-		    }
-
-                        
-		}
-		else {
-                    // retrieve a Random computer move    
-		    // generate a delay to slow computer down
-		    try{
-			Thread.sleep(500);
-			b.simpleComputerMove();
-		    }catch (InterruptedException ex1){
-			System.out.println("Broken Thread");
-		    }
-
-		}
-            
-            }
-            
-            //single player advanced
-            else if (gameMode == 3) {
                 if (b.getTurn() == 1){
                     xIndex = e.getX()/100;
                     yIndex = 0;
                     
                     
                     while(b.getGameGridCircle(xIndex, yIndex+1).getState() == 0)
-			{
-			    yIndex++;
-			    if (yIndex == b.numRows - 1) {
-				break;
-			    }
-			}
+                    {
+                        yIndex++;
+                        if (yIndex == b.numRows - 1) {
+                            break;
+                        }
+                    }
                     
                     //if the top circle is already filled, do nothing and return
                     if (yIndex == 0 && b.getGameGridCircle(xIndex, yIndex).getState() != 0) {
@@ -261,32 +201,45 @@ public class startScreen2 extends JFrame {
                     b.setDrawCounter(b.getDrawCounter()+1);
                     
                     
-                    //Make a automatic mouse click
-		    // to start the Computer Move
+                    //Make a automatic mouse click to start the Computer Move
                     try {
+                        // Thread.sleep(1000);
                         Robot r = new Robot();
                         r.mousePress(InputEvent.BUTTON1_MASK);
                         r.mouseRelease(InputEvent.BUTTON1_MASK);
                         
                     }catch (AWTException ex){
-                        System.out.println("generate robot didn't work");
+                        System.out.println("didn't work");
                     }
                     
                     
                 }
                 else {
-			//retrieve an advanced computer move
-			//generate a delay to slow computer down
-		    try{
-			Thread.sleep(500);
-			b.simpleComputerMove();
-		    }catch (InterruptedException ex1){
-			System.out.println("Broken Thread");
-		    }
-		}
-
-            }
-        }
+                    // retrieve a Random computer move
+                    // generate a delay to slow computer down
+                    if (gameMode == 2){
+                        try{
+                            Thread.sleep(500);
+                            SinglePlayerEasy.randomMove(b); //Generate a Random Move
+                        }catch (InterruptedException ex1){
+                            System.out.println("Broken Thread");
+                        }
+                    
+                    }
+                    else if (gameMode == 3){
+                        //retrieve an advanced computer move
+                        //generate a delay to slow computer down
+                        try{
+                            Thread.sleep(500);
+                            SinglePlayerAdvanced.AdvancedComputerMove(b);
+                        }catch (InterruptedException ex1){
+                            System.out.println("Broken Thread");
+                        }
+                    }// end else if (gameMode == 3)
+                
+                }// end else
+            }// end else
+        }// end Method
         
         public void mouseEntered(MouseEvent e) {
             // TODO Auto-generated method stub
@@ -296,7 +249,7 @@ public class startScreen2 extends JFrame {
         /**
          mouseExited is a function in the MouseListener interface
          @param e represents the mouseEvent
-	*/
+         */
         
         public void mouseExited(MouseEvent e) {
             // TODO Auto-generated method stub
@@ -306,7 +259,7 @@ public class startScreen2 extends JFrame {
         /**
          mousePressed is a function in the MouseListener interface
          @param e represents the mouseEvent
-	*/
+         */
         
         public void mousePressed(MouseEvent e) {
             //TODO Auto-generated method stub
@@ -315,215 +268,25 @@ public class startScreen2 extends JFrame {
         /**
          mouseReleased is a function in the MouseListener interface
          @param e represents the mouseEvent
-	*/
+         */
         
         public void mouseReleased(MouseEvent e) {
             // TODO Auto-generated method stub
         }
-
-        
-    }
-
-    // When the main Menu button is pressed
-    // return back to the startScreen
-    class mainMenuButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent event){
-            BackToStartScreen();
-            remove(b);
-            remove(inGameMenuP);
-        }
-    }
-    
-  
-    
-    // A JPanel to Layout the Buttons in Start Menu
-    
-    public class StartScreenButtonsPanel extends JPanel{
-        
-        private JButton MPButton;  // Multi Player Button
-        private JButton SPButton;  // Single Player Button
-        private JButton ExitButton;  // Exit Button
-        private JButton ruleButton;  // Button to show rules
-        
-        public StartScreenButtonsPanel(){
-            super();
-            
-            this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-            
-            Font BFont = new Font("Comic Sans MS", Font.BOLD, 22);
-            
-            MPButton = new JButton ("Multiplayer!");
-            MPButton.addActionListener(new MPButtonListener());
-            MPButton.setFont(BFont);
-            
-            SPButton = new JButton ("Single Player");
-            SPButton.addActionListener(new SPButtonListener());
-            SPButton.setFont(BFont);
-            
-            ruleButton = new JButton ("Rules");
-	    ruleButton.addActionListener(new ruleButtonListener());
-	    ruleButton.setFont(BFont);
-
-	    ExitButton = new JButton ("Exit");
-            ExitButton.addActionListener(new ExitButtonListener());
-            ExitButton.setFont(BFont);
-
-	    
-            
-            this.add(SPButton);
-            this.add(MPButton);
-	    this.add(ruleButton);
-            this.add(ExitButton);
-            
-        }
-
-	/*
-	public void displayRules(Graphics g){
-	    g.setColor(Color.BLACK);
-	    g.setFont(new Font("Times", font.REGULAR, 18));
-	    g.drawString("1.Choose a Column to Place a Circle in.\n2. Try to get Four identical circles in a row.");
-
-	}
-	*/
-        
-        class MPButtonListener implements ActionListener {
-            public void actionPerformed(ActionEvent event) {
-                gameMode = 1;
-                launchGame();
-            }
-        }
-        
-        class SPButtonListener implements ActionListener {
-            public void actionPerformed(ActionEvent event) {
-                System.out.println("Single Player Mode!");
-		//        b.setSinglePlayer(true);
-                loadSinglePlayerMenu();
-		//   launchGame();
-            }
-        }
-
-	class ruleButtonListener implements ActionListener {
-            public void actionPerformed(ActionEvent event){
-		//   displayRules(g); 
-            }
-        }
-        
-        class ExitButtonListener implements ActionListener {
-            public void actionPerformed(ActionEvent event) {
-                setVisible(false);
-                dispose();
-            }
-            
-        }
-        
-        
         
         
     }
+ 
     
-    //Upon Choosing Single Player, This will be the Panel that will show up
-
-    public class singlePlayerMenuPanel extends JPanel{
-        private JButton singlePlayerEasy;
-        private JButton singlePlayerAdvanced;
-        
-        public singlePlayerMenuPanel(){
-            super ();
-            this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-            Font BFont = new Font("Comic Sans MS", Font.BOLD, 22);
-            
-            singlePlayerEasy = new JButton ("Easy");
-            singlePlayerEasy.addActionListener(new singlePlayerEasyListener() );
-            singlePlayerEasy.setFont(BFont);
-            
-            singlePlayerAdvanced = new JButton ("Advanced");
-            singlePlayerAdvanced.addActionListener(new singlePlayerAdvancedListener() );
-            singlePlayerAdvanced.setFont(BFont);
-            
-            this.add(singlePlayerEasy);
-            this.add(singlePlayerAdvanced);
-        }
-        
-        class singlePlayerEasyListener implements ActionListener {
-            public void actionPerformed(ActionEvent event){
-                gameMode = 2;
-                launchGame();
-            }
-        }
-        
-        class singlePlayerAdvancedListener implements ActionListener {
-            public void actionPerformed(ActionEvent event){
-                gameMode = 3;
-		launchGame();
-           
-            }
-        }
+    //getters and Setters
+    public int getGameMode(){
+        return gameMode;
+    }
+    
+    public void setGameMode(int gMode){
+        gameMode = gMode;
     }
     
     
-    // Panel for Buttons next to Connect Four Game Board
-    
-    public class inGameMenuPanel extends JPanel{
-        private JButton mainMButton;  // main Menu Button
-        private JButton restartButton;  // restart Button
-        private JButton exitButton;   // exitButton
-	
-        public inGameMenuPanel(){
-            super ();
-            this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-            Font BFont = new Font ("Comic Sans MS", Font.BOLD, 22);
-        
-	    // Create a 
-	    // main button
-	    // restart button
-	    // exit Button
-            mainMButton = new JButton ("Main Menu");
-            mainMButton.addActionListener(new mainMenuButtonListener());
-            mainMButton.setFont(BFont);
-        
-            restartButton = new JButton ("Restart");
-            restartButton.addActionListener(new restartButtonListener());
-            restartButton.setFont(BFont);
-        
-            exitButton = new JButton ("Exit");
-            exitButton.addActionListener(new exitButtonListener());
-            exitButton.setFont(BFont);
-        
-	    
-        
-            this.add(mainMButton);
-            this.add(restartButton);
-	    
-            this.add(exitButton);
-
-        }
-	
-	// Listeners for Buttons in Panel
-        
-	// when restart Button is pressed
-	// relaunch Game
-        class restartButtonListener implements ActionListener {
-            public void actionPerformed(ActionEvent event){
-                launchGame();
-            }
-        }// end of RestartButtonListener Class
-
-	// Navigate back to startScreen when this button is pressed
-        class mainMenuButtonListener implements ActionListener {
-            public void actionPerformed(ActionEvent event){
-		BackToStartScreen();                
-            }
-        }// end of mainMenuButtonListener Class
-	
-	// when exit button is pressed 
-	// make the screen no longer visible, and stop execution
-        class exitButtonListener implements ActionListener {
-            public void actionPerformed(ActionEvent event){
-		setVisible(false);
-                dispose();
-            }
-        }// end of exitButtonListener Class
-    }// end of inGameMenuPanel Class
     
 }// end of startScreen Class
-
